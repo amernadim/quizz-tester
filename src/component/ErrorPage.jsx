2c05e0874daa4fbd33e7d64d88ad769f00b2eaef
Your Name <you@example.com>
2022-10-10 23:54:01 +0600

    added Error Page

--- src/component/ErrorPage.jsx
+++ src/component/ErrorPage.jsx
@@ -6,9 +6,9 @@ const ErrorPage = () => {
   // console.error(error);
   return (
     <div className='flex flex-col justify-center items-center mt-20'>
-      <h1 className='text-2xl text-gray-500'>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p className='text-red-600'>
+      <h1 className='text-6xl text-gray-500'>Oops!</h1>
+      <p className='text-4xl mt-4'>Sorry, an unexpected error has occurred.</p>
+      <p className='text-red-600 text-3xl mt-4'>
         <i>{error.statusText || error.message}</i>
       </p>
     </div>
