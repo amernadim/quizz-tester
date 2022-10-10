9488175671cfcecec1b8d145714cc3d8dc62bd0d
Your Name <you@example.com>
2022-10-11 00:36:18 +0600

    Error Page Added

--- src/component/ErrorPage.jsx
+++ src/component/ErrorPage.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { useRouteError } from 'react-router-dom';
+import React from "react";
+import { useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
   const error = useRouteError();
   // console.error(error);
   return (
-    <div className='flex flex-col justify-center items-center mt-20'>
-      <h1 className='text-6xl text-gray-500'>Oops!</h1>
-      <p className='text-4xl mt-4'>Sorry, an unexpected error has occurred.</p>
-      <p className='text-red-600 text-3xl mt-4'>
+    <div className="flex flex-col justify-center items-center mt-20">
+      <h1 className="text-6xl text-gray-500">Oops!</h1>
+      <p className="text-4xl mt-4">Sorry, an unexpected error has occurred.</p>
+      <p className="text-red-600 text-3xl mt-4">
         <i>{error.statusText || error.message}</i>
       </p>
     </div>
